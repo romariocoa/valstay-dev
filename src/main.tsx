c5a465582c1834885563501db60f009290bb87be
romariocoa <119425038+romariocoa@users.noreply.github.com>
2026-07-15 10:45:50 -0500

    feat: add Vercel Web Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import { LandingPage } from './components/LandingPage.tsx';
 import { InviteRegistration } from './components/InviteRegistration.tsx';
@@ -18,6 +19,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       {window.location.pathname === '/inicio' ? <LandingPage /> : window.location.pathname === '/registro' ? <InviteRegistration /> : <App />}
+      <Analytics />
     </ThemeProvider>
   </StrictMode>
 );
